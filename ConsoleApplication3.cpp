ac3b92a0a178bf41bccae300c84e5d7ecbbdd04c
Stanislav <Stanislav@yandex.ru>
2024-05-01 12:36:59 +0400

    bf test merge

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -3,22 +3,22 @@
 
 int main()
 {
-	int k1 = 0;
+	int kur1 = 0;
 	printf("Please, input number K1: ");
-	scanf_s("%d", &k1);
-	int k2 = 0;
+	scanf_s("%d", &kur1);
+	int kur2 = 0;
 	printf("Please, input number K2: ");
-	scanf_s("%d", &k2);
-	int k3 = 0;
+	scanf_s("%d", &kur2);
+	int kur3 = 0;
 	printf("Please, input number K3: ");
-	scanf_s("%d", &k3);
+	scanf_s("%d", &kur3);
 
 	int sum = 0;
 	int a = 0;
 	printf("Please, input number A: ");
 	scanf_s("%d", &a);
-	if (a >= k2 && a <= k3) {
-		if (!(a % k1)) {
+	if (a >= kur2 && a <= kur3) {
+		if (!(a % kur1)) {
 			sum = sum + a;
 		}
 	}
@@ -26,8 +26,8 @@ int main()
 	int b = 0;
 	printf("Please, input number B: ");
 	scanf_s("%d", &b);
-	if (b >= k2 && a <= k3) {
-		if (!(b % k1)) {
+	if (b >= kur2 && a <= kur3) {
+		if (!(b % kur1)) {
 			sum = sum + b;
 		}
 	}
@@ -35,8 +35,8 @@ int main()
 	int c = 0;
 	printf("Please, input number C: ");
 	scanf_s("%d", &c);
-	if (c >= k2 && c <= k3) {
-		if (!(c % k1)) {
+	if (c >= kur2 && c <= kur3) {
+		if (!(c % kur1)) {
 			sum = sum + c;
 		}
 	}
@@ -44,8 +44,8 @@ int main()
 	int d = 0;
 	printf("Please, input number D: ");
 	scanf_s("%d", &d);
-	if (d >= k2 && d <= k3) {
-		if (!(d % k1)) {
+	if (d >= kur2 && d <= kur3) {
+		if (!(d % kur1)) {
 			sum = sum + d;
 		}
 	}
